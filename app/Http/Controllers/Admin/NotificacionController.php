b58798b337132f87318d97efd09acdb8c66eca67
raulpsl <raulparedesld@gmail.com>
2024-06-29 08:04:11 -0400

    Espero que sea el ultimo commit :v

--- app/Http/Controllers/Admin/NotificacionController.php
+++ app/Http/Controllers/Admin/NotificacionController.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\scripts\EncontrarTodo;
 use App\Models\Admin\Docente;
 use App\Models\Admin\Notificacion as AdminNotificacion;
 use App\Notifications\Notificacion;
+use Carbon\Carbon;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Notification;
 use Ramsey\Uuid\Uuid;
 
@@ -20,17 +23,23 @@ class NotificacionController extends Controller
      */
     public function index()
     {
-        $notificaciones = AdminNotificacion::with('docente_relacion_notificacion')->get();
+        $notificaciones = AdminNotificacion::with('docente_relacion_notificacion')->where('CUERPO', 'LIKE', '%PENDIENTE%')->get();
         $notificaciones_estructuradas = [];
         foreach ($notificaciones as $notify) {
+            $hora_actual = new DateTime();
             $datos_docente = $notify['docente_relacion_notificacion'];
+            $cuerpo = $this->quitarCaracteres($notify['CUERPO']);
+            preg_match('/FECHA:\s*([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})/', $cuerpo, $matches);
+            $fecha_reserva = DateTime::createFromFormat('Y-m-d H:i', $matches[1]);
             if(isset($datos_docente)){
-                $notificaciones_estructuradas [] = [
-                    'ID' => $notify['ID_NOTIFICACION'],
-                    'CUERPO' => $this->quitarCaracteres($notify['CUERPO']),
-                    'NOMBRE_DOCENTE' => $datos_docente['NOMBRE'],
-                    'EMAIL' => $datos_docente['EMAIL']
-                ];
+                if($fecha_reserva > $hora_actual){
+                    $notificaciones_estructuradas [] = [
+                        'ID' => $notify['ID_NOTIFICACION'],
+                        'CUERPO' => $cuerpo,
+                        'NOMBRE_DOCENTE' => $datos_docente['NOMBRE'],
+                        'EMAIL' => $datos_docente['EMAIL']
+                    ];
+                }
             }
         }
         return view('admin.mail.mailbox', ['notificationes'=>$notificaciones_estructuradas]);
@@ -44,6 +53,10 @@ class NotificacionController extends Controller
         return $cadena;
     }
 
+    function reservasActuales(){
+
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -53,6 +66,7 @@ class NotificacionController extends Controller
     public function store(Request $request)
     {
         $buscador = new EncontrarTodo();
+        $fecha_actual = Date::now()->format('Y-m-d');
         $request = json_decode($request->getContent(), true);
         $nombres = json_decode($request['NOMBRES']);
         try {
@@ -63,7 +77,7 @@ class NotificacionController extends Controller
                 Notification::route('mail', (($docente->EMAIL != '') ? $docente->EMAIL :'nombre@universidad.edu.bo'))->notify(new Notificacion($request['TIPO'], ['NOMBRE' => $nombre, 'ESTADO' => (!isset($request['ESTADO']) ? 'PENDIENTE' : $request['ESTADO']),'FECHA' => $request['FECHA'], 'MATERIA' => $request['MATERIA'], 'AMBIENTE' => $request['AMBIENTE'], 'RAZONES' => (isset($request['RAZONES'])) ? $request['RAZONES']['LISTA_REG'] : '']));
                 AdminNotificacion::create([
                     'ID_NOTIFICACION' => Uuid::uuid4(),
-                    'CUERPO' => json_encode(['FECHA' => $request['FECHA'], 'MATERIA' => $request['MATERIA'], 'AMBIENTE' => $request['AMBIENTE'], 'ESTADO' => (!isset($request['ESTADO']) ? 'PENDIENTE' : $request['ESTADO']), 'TIPO' => $request['TIPO']]),
+                    'CUERPO' => json_encode(['FECHA' => $request['FECHA'], 'MATERIA' => $request['MATERIA'], 'AMBIENTE' => $request['AMBIENTE'], 'ESTADO' => (!isset($request['ESTADO']) ? 'PENDIENTE' : $request['ESTADO']), 'TIPO' => $request['TIPO'], 'MODO' => ($request['FECHA'] < $fecha_actual) ? 'URGENTE':'NORMAL']),
                     'ID_DOCENTE' => $docenteId
                 ]);
             }
